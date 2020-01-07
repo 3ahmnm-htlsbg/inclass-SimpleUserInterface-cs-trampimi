31a53f4feac830c901396d45c6c179ae1903ab7b
unknown <mira.trampitsch02@htl-salzburg.ac.at>
2020-01-07 15:03:33 +0100

    add script

---
+++ Assets/MyGame/Script/interactionScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class interactionScript : MonoBehaviour
+{
+    public Text NameOut;
+    public Text CountOut;
+    public Button Namebtn;
+    public Button Countbtn;
+    public InputField NameIn;
+    public int CountNumber = 0;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetName()
+    {
+        NameOut.text = NameIn.text;
+
+    }
+
+    public void CountUp()
+    {
+        CountNumber++;
+        CountOut.text = CountNumber.ToString();
+    }
+
+}
